62f8a0ce1796454b44d8a010537cedab3a5ca8df
Nathan Osman <admin@quickmediasolutions.com>
2013-09-11 16:01:25 -0700

    Added initial implementation of Promise class.

--- include/nitroshare/util/promise.h
+++ include/nitroshare/util/promise.h
@@ -12,6 +12,7 @@
 #define NSU_PROMISE_H
 
 #include <QObject>
+#include <QVariantMap>
 
 #include "export.h"
 
@@ -19,17 +20,73 @@ namespace NitroShare
 {
     namespace Util
     {
+        /**
+         * @brief An implementation of the promise pattern
+         *
+         * The Promise class represents an asynchronous task such as a network
+         * request or computation. Each instance of this class can be used to
+         * cancel the pending operation or receive notification when it
+         * completes (by connecting to the completed() signal).
+         */
         class NSU_EXPORT Promise : public QObject
         {
             Q_OBJECT
             
             public:
             
-                Promise(QObject * parent = nullptr);
-                virtual ~Promise();
+                /**
+                 * @brief Creates a Promise object
+                 * @param delete_on_finished whether the promise should delete itself after the finished signal is emitted
+                 * @param parent the parent QObject
+                 *
+                 * Note: if you are creating a Promise object on the stack, you
+                 * must specify false for delete_on_finished.
+                 */
+                explicit Promise(bool delete_on_finished = true, QObject * parent = nullptr);
+                
+                /**
+                 * @brief Waits for the promise to finish
+                 * @return true if the promise completed
+                 *
+                 * Although this method blocks, it creates an event loop to
+                 * allow for GUI repainting, etc.
+                 */
+                bool waitForFinished();
+
+            Q_SIGNALS:
+
+                /**
+                 * @brief Emitted as progress is made
+                 * @param value an integer between 0 and 100 inclusive
+                 */
+                void progress(int value);
+
+                /**
+                 * @brief Emitted when the promise has been canceled
+                 */
+                void canceled();
+
+                /**
+                 * @brief Emitted when an error occurs
+                 * @param message a message describing the error
+                 */
+                void error(const QString & message);
+
+                /**
+                 * @brief Emitted when the promise has completed
+                 * @param parameters any data expected to be returned by the task
+                 */
+                void completed(const QVariantMap & parameters = QVariantMap());
+
+                /**
+                 * @brief Emitted when the promise has finished
+                 *
+                 * This signal will always be emitted after the canceled(),
+                 * error(), or completed() signal has been emitted.
+                 */
+                void finished();
         };
     }
 }
 
 #endif // NSU_PROMISE_H
-
--- src/promise.cpp
+++ src/promise.cpp
@@ -8,15 +8,34 @@
  * License, or (at your option) any later version.
  */
 
+#include <QEventLoop>
+
 #include <nitroshare/util/promise.h>
 
 using namespace NitroShare::Util;
 
-Promise::Promise(QObject * parent)
+Promise::Promise(bool delete_on_finished, QObject * parent)
     : QObject(parent)
 {
+    connect(this, &Promise::canceled,  this, &Promise::finished);
+    connect(this, &Promise::error,     this, &Promise::finished);
+    connect(this, &Promise::completed, this, &Promise::finished);
+
+    if(delete_on_finished)
+        connect(this, &Promise::finished, this, &Promise::deleteLater);
 }
 
-Promise::~Promise()
+bool Promise::waitForFinished()
 {
+    QEventLoop loop;
+    bool completed = false;
+
+    connect(this, &Promise::completed, [&completed]()
+    {
+        completed = true;
+    });
+    connect(this, &Promise::finished, &loop, &QEventLoop::quit);
+
+    loop.exec();
+    return completed;
 }
