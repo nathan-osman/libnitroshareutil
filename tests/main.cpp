4125b59120a3939488be97abf499300a3cfc5c3f
Nathan Osman <admin@quickmediasolutions.com>
2013-09-14 08:24:21 -0700

    Added build file for test suite.

---
+++ tests/main.cpp
@@ -0,0 +1,14 @@
+/*
+ * libnitroshareutil
+ * Copyright (C) 2013  Nathan Osman
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 3 of the
+ * License, or (at your option) any later version.
+ */
+
+int main(int argc, char * argv[])
+{
+    return 0;
+}
