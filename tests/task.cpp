ed41ed4763367dd886ad7805448f78f5935eca04
Nathan Osman <admin@quickmediasolutions.com>
2013-09-29 00:18:19 -0700

    Corrected license in comment block in test files and implemented FileHeader test.

--- tests/task.cpp
+++ tests/task.cpp
@@ -1,19 +1,11 @@
 /*
- * NitroShare - Send Any File to Any Device
+ * libnitroshareutil
  * Copyright (C) 2013  Nathan Osman
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 3 of the
+ * License, or (at your option) any later version.
  */
 
 #include "task.h"
--- tests/testfileheader.cpp
+++ tests/testfileheader.cpp
@@ -1,24 +1,40 @@
 /*
- * NitroShare - Send Any File to Any Device
+ * libnitroshareutil
  * Copyright (C) 2013  Nathan Osman
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 3 of the
+ * License, or (at your option) any later version.
  */
 
+#include <QDir>
+#include <QFile>
+#include <QFileInfo>
+#include <QTest>
+
+#include <nitroshare/util/fileheader.h>
 #include "testfileheader.h"
+
+using namespace NitroShare::Util;
 
 void TestFileHeader::run()
 {
-    //...
+    QDir temp(QDir::temp());
+    QString filename(temp.absoluteFilePath("test.txt"));
+
+    // Create a file that is writable and executable
+    QFile file(filename);
+    QVERIFY(file.open(QIODevice::WriteOnly));
+    QVERIFY(file.write("42"));
+    file.close();
+
+    FileHeader header(QFileInfo(filename));
+    QCOMPARE(header.absoluteFilename(), filename);
+    QCOMPARE(header.relativeFilename(), QString("test.txt"));
+    QCOMPARE(header.size(),             qint64(2));
+    QCOMPARE(header.writable(),         true);
+    // There is no platform-independent way to test executable()
+
+    temp.remove("test.txt");
 }
